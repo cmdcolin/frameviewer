399db0927fac827bdb6094e083b0a5eafb1155eb
Colin <colin.diesh@gmail.com>
2017-02-07 03:41:25 -0500

    Add more test data

---
+++ js/View/FeatureGlyph/Frames.js
@@ -0,0 +1,75 @@
+define([
+    'dojo/_base/declare',
+    'dojo/_base/array',
+    'dojox/color/Palette',
+    'JBrowse/Model/SimpleFeature',
+    'JBrowse/View/FeatureGlyph/ProcessedTranscript'
+],
+function (
+    declare,
+    array,
+    Palette,
+    SimpleFeature,
+    ProcessedTranscript
+) {
+    return declare(ProcessedTranscript, {
+        renderFeature: function (context, fRect) {
+            if (this.track.displayMode != 'collapsed') {
+                context.clearRect(Math.floor(fRect.l), fRect.t, Math.ceil(fRect.w), fRect.h);
+            }
+            
+
+            this.renderFrames(context,  fRect);
+            this.renderSegments(context, fRect);
+        },
+        renderSegments: function (context, fRect) {
+            var subparts = this._getSubparts(fRect.f);
+            if (!subparts.length) {
+                return;
+            }
+
+            var thisB = this;
+            var parentFeature = fRect.f;
+            function style(feature, stylename) {
+                if (stylename == 'height') {
+                    return thisB._getFeatureHeight(fRect.viewInfo, feature);
+                }
+                return thisB.getStyle(feature, stylename) || thisB.getStyle(parentFeature, stylename);
+            }
+
+            for (var i = 0; i < subparts.length; ++i) {
+                var start = subparts[i].get('start');
+                var frame = start % 3;
+                console.log(this.config.style.frameHeight)
+                this.renderBox(context, fRect.viewInfo, subparts[i], fRect.t + frame*this.config.style.frameHeight/3, this.config.style.frameHeight/3, fRect.f, style);
+            }
+        },
+
+        renderFrames: function( context, fRect ) {
+            // connector
+            var connectorColor = this.getStyle( fRect.f, 'connectorColor' );
+            if( connectorColor ) {
+                context.fillStyle = connectorColor;
+                var connectorThickness = this.getStyle( fRect.f, 'connectorThickness' );
+                context.fillRect(
+                    fRect.rect.l, // left
+                    Math.round(fRect.rect.t), // top
+                    fRect.rect.w, // width
+                    connectorThickness
+                );
+                context.fillRect(
+                    fRect.rect.l, // left
+                    Math.round(fRect.rect.t+this.config.style.frameHeight/3), // top
+                    fRect.rect.w, // width
+                    connectorThickness
+                );
+                context.fillRect(
+                    fRect.rect.l, // left
+                    Math.round(fRect.rect.t+this.config.style.frameHeight*2/3), // top
+                    fRect.rect.w, // width
+                    connectorThickness
+                );
+            }
+        }
+    });
+});
---
+++ js/View/Track/Frames.js
@@ -0,0 +1,25 @@
+define([
+    'dojo/_base/declare',
+    'dojo/_base/array',
+    'dojo/_base/lang',
+    'JBrowse/View/Track/CanvasFeatures',
+    'JBrowse/Util'
+],
+function (
+    declare,
+    array,
+    lang,
+    CanvasFeatures,
+    Util
+) {
+    return declare(CanvasFeatures, {
+        _defaultConfig: function() {
+            return Util.deepUpdate(lang.clone(this.inherited(arguments)), {
+                glyph: 'FrameViewer/View/FeatureGlyph/Frames',
+                style: {
+                    frameHeight: 40
+                }
+            });
+        }
+    });
+});
