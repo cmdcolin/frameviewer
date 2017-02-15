088c99832f077f0d793d3553117be3e8dc16bc99
Colin <colin.diesh@gmail.com>
2017-02-15 18:46:59 -0500

    Add protein and dna rendering onto feature

--- js/View/FeatureGlyph/Frames.js
+++ js/View/FeatureGlyph/Frames.js
@@ -1,19 +1,26 @@
 define([
     'dojo/_base/declare',
     'dojo/_base/array',
+    'dojo/_base/lang',
     'dojox/color/Palette',
     'JBrowse/Model/SimpleFeature',
-    'JBrowse/View/FeatureGlyph/ProcessedTranscript'
+    'JBrowse/View/FeatureGlyph/ProcessedTranscript',
+    'JBrowse/CodonTable'
 ],
 function (
     declare,
     array,
+    lang,
     Palette,
     SimpleFeature,
-    ProcessedTranscript
+    ProcessedTranscript,
+    CodonTable
 ) {
     return declare(ProcessedTranscript, {
         renderFeature: function (context, fRect) {
+            var c = new CodonTable();
+            var r = c.generateCodonTable(lang.mixin(c.defaultCodonTable, this.track.browser.config.codonTable));
+
             var viewInfo = fRect.viewInfo;
             var fh = this.config.style.frameHeight;
             context.clearRect(Math.floor(fRect.l), fRect.t, Math.ceil(fRect.w), fRect.h);
@@ -21,18 +28,51 @@ function (
             this.renderFrames(context, fRect);
 
             var subparts = this._getSubparts(fRect.f);
-            var end = this.track.browser.refSeq.length;
+            var thisB = this;
+
+            var drawDNA = function (left, top, del) {
+                return function (seq) {
+                    for (var j = 0; j < seq.length; j++) {
+                        context.fillStyle = 'white';
+                        context.fillText(seq[j], left + j * del, top);
+                    }
+                };
+            }
+
+            var drawProtein = function (left, top, del) {
+                return function (seq) {
+                    for (var j = 0; j < seq.length; j+=3) {
+                        context.fillStyle = 'white';
+                        context.fillText(r[seq.substring(j,j+3)], left + j * del, top);
+                    }
+                };
+            }
 
             for (var i = 0; i < subparts.length; ++i) {
                 var s = subparts[i];
+                context.fillStyle = 'black';
                 if (s.get('type') === 'CDS') {
-                    var frame = s.get('strand') == 1 ?
+                    var frame = s.get('strand') === 1 ?
                         (s.get('start') % 3) :
                         (s.get('end') % 3);
 
                     var left  = viewInfo.block.bpToX(s.get('start'));
+                    var right  = viewInfo.block.bpToX(s.get('start') + 1);
+                    var delta = right - left;
                     var width = viewInfo.block.bpToX(s.get('end')) - left;
-                    context.fillRect(left, fRect.t + fh * (frame+1) / 4 - fh / 12, Math.max(1, width), fh / 6);
+                    context.fillRect(left, fRect.t + fh * (frame + 1) / 4 - fh / 12, Math.max(1, width), fh / 6);
+                    context.font = '8px';
+                    if (this.config.showDNA && viewInfo.block.scale > 5) {
+                        this.track.browser.getStore('refseqs', function (store) {
+                            store.getReferenceSequence({ ref: s.get('seq_id'), start: s.get('start'), end: s.get('end') }, drawDNA(left, fRect.t + fh * (frame + 1) / 4 + fh / 16, delta));
+                        });
+                    }
+                    if (this.config.showProtein && viewInfo.block.scale > 5) {
+                        this.track.browser.getStore('refseqs', function (store) {
+                            context.fillStyle = 'white';
+                            store.getReferenceSequence({ ref: s.get('seq_id'), start: s.get('start'), end: s.get('end') }, drawProtein(left, fRect.t + fh * (frame + 1) / 4 + fh / 16, delta));
+                        });
+                    }
                 }
             }
         },
@@ -67,5 +107,7 @@ function (
         _getFeatureHeight: function () {
             return this.config.style.frameHeight;
         }
+        
+        
     });
 });
