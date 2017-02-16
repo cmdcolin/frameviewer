0129d0d09cf7eb24e0ce27b541e7457337b7f019
Colin <colin.diesh@gmail.com>
2017-02-15 21:53:44 -0500

    Protein translation remainder needs to include phase

--- js/View/FeatureGlyph/Frames.js
+++ js/View/FeatureGlyph/Frames.js
@@ -32,114 +32,90 @@ function (
 
             var subparts = this._getSubparts(feature);
 
-            var drawDNA = function (left, top, del, feat) {
+            var drawDNA = function (feat, left, top, del) {
                 return function (seq) {
-                    if(feat.get('strand') == -1) {
-                        seq = Util.complement(seq);
+                    var newseq = seq;
+                    if (feat.get('strand') == -1) {
+                        newseq = Util.complement(seq);
                     }
-                    for (var j = 0; j < seq.length; j++) {
+                    for (var j = 0; j < newseq.length; j++) {
                         context.fillStyle = 'white';
-                        context.fillText(seq[j], left + j * del, top);
+                        context.fillText(newseq[j], left + j * del, top);
                     }
                 };
             };
 
-            if(this.config.showProtein) {
-                this.track.browser.getStore('refseqs', function (store) {
-                    context.fillStyle = 'white';
-                    var fstart = feature.get('start');
-                    var fend = feature.get('end');
-                    store.getReferenceSequence({ ref: feature.get('seq_id'), start: fstart, end: fend }, function(seq) {
-                        var s = '';
-                        var ret = {};
-                        if(feature.get('strand') == -1) {
-                            subparts.forEach(function(s) {
-                                ret[s.get('start')] = Util.revcom(seq.substring(s.get('start') - fstart, s.get('end') - fstart));
-
-                            });
-                            Object.keys(ret).sort(function(a, b) { return a < b; }).forEach(function(elt) {
-                                s += ret[elt];
-                            });
-                        }
-                        else {
-                            subparts.forEach(function(s) {
-                                ret[s.get('start')] = seq.substring(s.get('start') - fstart, s.get('end') - fstart);
-
-                            });
-                            Object.keys(ret).sort(function(a, b) { return a < b; }).forEach(function(elt) {
-                                s += ret[elt];
-                            });
-                        }
-                        if(thisB.config.showProtein) {
-
-                        }
-                    });
-                    //drawProtein(left, fRect.t + fh * (frame + 1) / 4 + fh / 16, delta, s));
-                });
-
-            }
-
-            var drawProtein = function (left, top, del, feat) {
+            var drawProtein = function (feat, left, top, del) {
                 this.prev = '';
                 var thisB = this;
                 return function (seq) {
                     var n = Math.floor(seq.length / 3) * 3;
-                    var remainder = seq.length % 3;
+                    var remainder = (seq.length+prev.length) % 3;
                     var phase = feat.get('phase');
-                    if(feat.get('strand') == -1) {
-                        if (this.prev) {
+                    var newseq = seq;
+
+                    if (feat.get('strand') == -1) {
+                        newseq = Util.revcom(seq);
+                        if (thisB.prev) {
                             context.fillStyle = 'black';
-                            context.fillText(codons[Util.revcom(seq.substring(0, phase)) + thisB.prev], left - (3 - phase) * del, top);
-                        }
-                        for (var j = seq.length; j > 0; j -= 3) {
-                            context.fillStyle = 'white';
-                            var ret = Util.revcom(seq.substring(j - 3, j));
-                            context.fillText(codons[ret], left + (j-1) * del, top);
-                        }
-                        if (remainder) {
-                            thisB.prev = Util.revcom(seq.substring(seq.length - remainder, seq.length));
-                        }
-                    } else {
-                        if (this.prev) {
-                            context.fillStyle = 'black';
-                            context.fillText(codons[thisB.prev + seq.substring(0, phase)], left - (3 - phase) * del, top);
+                            context.fillText(codons[Util.revcom(newseq.substring(0, phase)) + thisB.prev], left - (3 - phase) * del, top);
+                            thisB.prev = '';
                         }
                         for (var j = phase; j < n; j += 3) {
                             context.fillStyle = 'white';
-                            context.fillText(codons[seq.substring(j, j + 3)], left + j * del, top);
+                            var ret = newseq.substring(j, j + 3);
+                            context.fillText(codons[ret], left + (newseq.length - j - 1) * del, top);
                         }
                         if (remainder) {
-                            thisB.prev = seq.substring(seq.length - remainder, seq.length);
+                            thisB.prev = Util.revcom(newseq.substring(newseq.length - remainder, newseq.length));
+                        }
+                    } else {
+                        if (thisB.prev && phase) {
+                            context.fillStyle = 'black';
+                            context.fillText(codons[thisB.prev + newseq.substring(0, phase)], left - (3 - phase) * del, top);
+                            thisB.prev = '';
+                        }
+                        for (var j = phase; j < n; j += 3) {
+                            context.fillStyle = 'white';
+                            context.fillText(codons[newseq.substring(j, j + 3)], left + j * del, top);
+                        }
+                        if (remainder) {
+                            thisB.prev = newseq.substring(newseq.length - remainder, newseq.length);
                         }
                     }
                 };
             };
+
+            if (feature.get('strand') == -1) {
+                subparts = subparts.sort(function (a, b) { return b.get('start') > a.get('start'); });
+            }
 
             for (var i = 0; i < subparts.length; ++i) {
                 var s = subparts[i];
                 context.fillStyle = 'black';
                 if (s.get('type') === 'CDS') {
-                    var frame = s.get('strand') === 1 ?
-                        (s.get('start') % 3) :
-                        (s.get('end') % 3);
-
+                    var frame = s.get('strand') === 1 ? (s.get('start') % 3) : (s.get('end') % 3);
                     var left  = viewInfo.block.bpToX(s.get('start'));
                     var right  = viewInfo.block.bpToX(s.get('start') + 1);
                     var delta = right - left;
                     var width = viewInfo.block.bpToX(s.get('end')) - left;
                     context.fillRect(left, fRect.t + fh * (frame + 1) / 4 - fh / 12, Math.max(1, width), fh / 6);
                     context.font = '8px';
-//                    if (this.config.showDNA && viewInfo.block.scale > 5) {
-//                        this.track.browser.getStore('refseqs', function (store) {
-//                            store.getReferenceSequence({ ref: s.get('seq_id'), start: s.get('start'), end: s.get('end') }, drawDNA(left, fRect.t + fh * (frame + 1) / 4 + fh / 16, delta, s));
-//                        });
-//                    }
-//                    if (this.config.showProtein && viewInfo.block.scale > 3) {
-//                        this.track.browser.getStore('refseqs', function (store) {
-//                            context.fillStyle = 'white';
-//                            store.getReferenceSequence({ ref: s.get('seq_id'), start: s.get('start'), end: s.get('end') }, drawProtein(left, fRect.t + fh * (frame + 1) / 4 + fh / 16, delta, s));
-//                        });
-//                    }
+                    if (this.config.showDNA && viewInfo.block.scale > 5) {
+                        this.track.browser.getStore('refseqs', (function(f, l, t, d) {
+                            return function (store) {
+                                store.getReferenceSequence({ ref: f.get('seq_id'), start: f.get('start'), end: f.get('end') }, drawDNA(f, l, t, d));
+                            }
+                        })(s, left, fRect.t + fh * (frame + 1) / 4 + fh / 16, delta));
+                    }
+                    if (this.config.showProtein && viewInfo.block.scale > 3) {
+                        this.track.browser.getStore('refseqs', (function(f, l, t, d) {
+                            context.fillStyle = 'white';
+                            return function (store) {
+                                store.getReferenceSequence({ ref: f.get('seq_id'), start: f.get('start'), end: f.get('end') }, drawProtein(f, l, t, d));
+                            }
+                        })(s, left, fRect.t + fh * (frame + 1) / 4 + fh / 16, delta));
+                    }
                 }
             }
         },
@@ -152,9 +128,9 @@ function (
                 context.fillStyle = connectorColor;
                 var connectorThickness = this.getStyle(fRect.f, 'connectorThickness');
                 context.fillRect(
-                    fRect.rect.l, // left
-                    Math.round(fRect.rect.t + this.config.style.frameHeight / 4), // top
-                    fRect.rect.w, // width
+                    fRect.rect.l,
+                    Math.round(fRect.rect.t + this.config.style.frameHeight / 4),
+                    fRect.rect.w,
                     connectorThickness
                 );
                 context.fillRect(
